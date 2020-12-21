0c0dac6c7654343630a49dfa0a6874413590ff76
Scatmandu <mcclur05@nsuok.edu>
2020-12-21 19:53:20 +0000

    changed prototype to use argc argv

--- main.c
+++ main.c
@@ -1,26 +1,35 @@
 #include "monty.h"
-int main(void)
+int main(int argc, char **argv)
 {
 	char buffer = NULL;
 	size_t bufsize = 0;
-	unsigned int line_number = 0;
+	unsigned int line_number = 1;
 	size_t line;
-	FILE *fp = fopen(*.m);
+	FILE *fd;
+	stack_t *head = NULL;
+	stack_t *tail = NULL;
 
-	if(!fp)
+	if(argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		return EXIT_FAILURE
 	}
-	line = _getline(stdin);
-	while (line >= 0)
+	
+	fd = fopen(argv[1], "r");
+
+	if (fd == NULL);
+	{
+		printf("Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (getline(&buffer, &bufsize, fd) != -1)
 	{
 		line_number++;
 		/*where we tokenize*/
-		line = getline(&buffer, &bufsize, fp);
+		line = getline(&buffer, &bufsize, fd);
 	}
 	free(buffer);
 	buffer = NULL;
-	fclose(fp);
+	fclose(fd);
 	return (EXIT_SUCCESS);
 }
