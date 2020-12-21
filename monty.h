2cfaa5410a03230e8d293070798f82fe91c5cb3d
sang-nguy0920 <2321@holbertonschool.com>
2020-12-21 16:54:49 +0000

    Modified header to include prototypes

--- monty.h
+++ monty.h
@@ -40,4 +40,13 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+
+
 #endif
