ca676398e7f827434765c6f2da6133bc56724544
Scatmandu <mcclur05@nsuok.edu>
2020-12-21 17:58:12 +0000

    Added main file

---
+++ main.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+int main(void)
+{
+	char buffer = NULL;
+	size_t bufsize = 0;
+	unsigned int line_number = 0;
+	size_t line;
+	FILE *fp = fopen(*.m);
+
+	if(!fp)
+	{
+		fprintf(stderr, "USAGE: monty file\n")
+		return EXIT_FAILURE
+	}
+	line = getline(&buffer, &bufsize, fp)
+	while (line >= 0)
+	{
+		line_number++;
+		/*where we tokenize*/
+		line = getline(&buffer, &bufsize, fp)
+	}
+	free(buffer);
+	buffer = NULL;
+	fclose(fp);
+	return (EXIT_SUCCESS);
+}
