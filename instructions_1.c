1eb7db02f3fff37d142b26695911265dbb0d7e37
Scatmandu <mcclur05@nsuok.edu>
2020-12-22 00:50:54 +0000

    added printfs to instructions

--- instructions_1.c
+++ instructions_1.c
@@ -16,6 +16,7 @@ void push(stack_t **stack, unsigned int line_number, char *n)
 		new_node->next = NULL;
 		new_node->prev = NULL;
 		head = new_node;
+	printf("%d\n", new_node->n);
 		exit(EXIT_SUCCESS); /* not sure about this */
 	}
 	else
@@ -31,6 +32,7 @@ void push(stack_t **stack, unsigned int line_number, char *n)
 		new_node->prev = NULL;
 		head->prev = new_node;
 		head = new_node;
+	printf("%d\n", new_node->n);
 		exit(EXIT_SUCCESS);
 	}
 	*stack = new_node;
