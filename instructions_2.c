eb99736513ef720368cdd814ffcdaf640a2c526d
Scatmandu <mcclur05@nsuok.edu>
2020-12-22 17:04:53 +0000

    wrong error message

--- instructions_2.c
+++ instructions_2.c
@@ -77,7 +77,7 @@ void _mod(stack_t **stack, unsigned int line_number)
 	}
 	if ((*stack == NULL) | ((*stack)->next == NULL))
 	{
-		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	pop(stack, line_number);
